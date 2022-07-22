9b15fe4bf35ba8d73d6f87ad8acbbf1b2b3ec970
RitaOC <onwudiwechinwendu16@gmail.com>
2022-07-22 17:30:24 +0100

    Additional changes

---
+++ JS/vote.js
@@ -0,0 +1,34 @@
+
+
+// const voteContent = document.getElementById('contestants-imgs');
+// const clickElon = document.querySelector('.elonsVote');
+// const clickEdward = document.querySelector('.edwardsVote');
+// let imgElon = document.querySelector('#elonsImg')
+
+// function vote() {
+//   if (vote.classlist.contains('elonsVote')) {
+//     alert('thank you')
+//   }
+// }
+
+
+// function elonsClick() {
+//   document.getElementById("elonsVote").click();
+//   imgElon.style.backgroundColor = 'red';
+//   console.log(clickElon);
+// }
+
+// const mapLoop = simplemaps_usmaps_mapdata.state_specific
+// function colorChange() {
+//   if (mapLoop[key].name === 'Alaska') {
+//     mapLoop.style.backgroundColor = 'red';
+//     // console.log('${key}:${colorChange[key]}');
+//   }
+// };
+// mapLoop.addEventListener("mouseover", colorChange)
+
+
+function elonsClick() {
+  alert('vote for elon is recorded')
+}
+
